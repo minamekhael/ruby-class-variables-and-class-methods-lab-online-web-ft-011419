2e129ee9167403aef51223ab801dfe7ade19860b
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-27 00:54:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,18 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres each do |genre|
-      if genre_count[genre] 
-        genre_count[genre]+= 1
-    else
-      genre_count[genre] = 1
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
     end
+    genre_count
   end
-  genre_count
-end
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
 end
