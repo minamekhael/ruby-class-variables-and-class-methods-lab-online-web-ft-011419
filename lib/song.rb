88ca365a37556e556decaa58854412e9bcdaa301
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-27 00:44:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@artists = {}
   @@genres = {}
   
-  def initialize(name, artist, genre)
+  def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
