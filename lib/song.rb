023acebb9f1a3cbb2e496e5371cc528773ebe2b0
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-27 00:31:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,10 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  @@song_count = 0
-  @@genres = []
-  @@artists = []
-  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@artists << artist
-    @@genres << genre
-    @count+= 1
   end
   
 end
